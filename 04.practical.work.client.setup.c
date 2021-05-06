e1919310c667b9ca5e78de7d63c3962e5f5b6c02
Hiếu Trịnh <78504039+qhieu01@users.noreply.github.com>
2021-05-06 10:57:28 +0700

    Update 04.practical.work.client.setup.c

--- 04.practical.work.client.setup.c
+++ 04.practical.work.client.setup.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]){
           i++;
        }
        printf("\n");
-}
+    }
     int sockfd;
     if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) <0){
       printf("Error creating socket\n");
